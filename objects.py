c353a393a115cc277904f7bd8247ade652a09b75
sicness <sicness@darklogic.ru>
2013-02-01 17:50:26 +0000

    Crontab class added
    Tested add and run :) Works. Good job.

--- objects.py
+++ objects.py
@@ -1,5 +1,9 @@
 # -*- coding: UTF-8 -*-
 
+import datetime
+from threading import Thread
+from time import sleep
+
 class Vars:
 	def	__init__(self):
 		""" Class for global vars """
@@ -20,6 +24,7 @@ class Vars:
 		""" return True if global var exist """
 		return var in self.vars
 
+
 class MotionSensor:
 	def __init__(self, state = 0):
 		self.state = state
@@ -38,3 +43,43 @@ class MotionSensor:
 				if self.onOff != None:
 					self.onOff()
 
+class Task:
+	def __init__(self, name, when, foo):
+		self.name = name
+		self.when = when
+		self.do = foo
+
+
+class Crontab:
+	def __init__(self):
+		self.tasks = list()
+		self.thread = Thread(target = self.check, args = ())
+		self.thread.start()
+
+	def add(self, task):
+		for t in self.tasks:
+			if t.name == task.name:			# If we alredy have task with this name
+				return -1
+		self.tasks.append(task)
+
+	def remove(self, name):
+		found = None
+		for task in self.tasks:
+			if task.name == name:
+				found = task
+		if found != None:
+			self.tasks.remove(found)
+
+	def check(self):
+		while True:
+			print('DEBUG: cron interation started')
+			now = datetime.datetime.now()
+			remove = list()
+			for task in self.tasks:
+				if task.when < now:
+					print('DEBUG: task.when < now')
+					task.do()									# TODO: Run in new thread!
+					remove.append(task)
+			for task in remove:
+				self.tasks.remove(task)
+			sleep(1)
