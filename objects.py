e873abf4cd20ec9b9f10879ca4f79e8162399baa
sicness <sicness@darklogic.ru>
2013-02-01 16:06:11 +0000

    Added class MotionSensor
    Not tested yet. Commit created for beging of new class creation ( crontab :evilsmile: )

--- objects.py
+++ objects.py
@@ -19,3 +19,22 @@ class Vars:
 	def exist(self, var):
 		""" return True if global var exist """
 		return var in self.vars
+
+class MotionSensor:
+	def __init__(self, state = 0):
+		self.state = state
+		self.onOn = None
+		self.onOff = None
+		self.onChange = None
+
+	def update(self, state):
+			self.state = state
+			if self.onChange != None:
+				self.onChange()
+			if state:
+				if self.onOn != None:
+					self.onOff()
+			else:
+				if self.onOff != None:
+					self.onOff()
+
