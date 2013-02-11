d0473e75b0ae4b6494bc879ecf7c7a4abcad5d0e
sicness <sicness@darklogic.ru>
2013-02-11 22:29:03 +0400

    Coming to PEP8 + noBodyHome
    Comming to PEP8 standart. Just started.
    Used class cron.
    Function for motion off and track when noBodyHome.
    Greeting on noBodyHome -> 0

--- objects.py
+++ objects.py
@@ -5,84 +5,90 @@ from threading import Thread
 from time import sleep
 
 class Vars:
-	def	__init__(self):
-		""" Class for global vars """
-		self.vars = dict()
+    def __init__(self):
+        """ Class for global vars """
+        self.vars = dict()
 
-	def get(self, var):
-		""" Get value of global var """
-		if var in self.vars:
-			return self.vars[var]
-		else:
-			return ''
+    def get(self, var):
+        """ Get value of global var """
+        if var in self.vars:
+            return self.vars[var]
+        else:
+            return ''
 
-	def set(self, name, value):
-		""" Set value of global var """
-		self.vars[name] = value
+    def set(self, name, value):
+        """ Set value of global var """
+        self.vars[name] = value
 
-	def exist(self, var):
-		""" return True if global var exist """
-		return var in self.vars
+    def exist(self, var):
+        """ return True if global var exist """
+        return var in self.vars
 
 
 class MotionSensor:
-	def __init__(self, state = 0):
-		self.state = state
-		self.onOn = None
-		self.onOff = None
-		self.onChange = None
+    def __init__(self, state = 0):
+        self.state = state
+        self.onOn = None
+        self.onOff = None
+        self.onChange = None
 
-	def update(self, state):
-			self.state = state
-			if self.onChange != None:
-				self.onChange()
-			if state:
-				if self.onOn != None:
-					self.onOn()
-			else:
-				if self.onOff != None:
-					self.onOff()
+    def update(self, state):
+            self.state = state
+            if self.onChange != None:
+                self.onChange()
+            if state:
+                if self.onOn != None:
+                    self.onOn()
+            else:
+                if self.onOff != None:
+                    self.onOff()
 
 class Task:
-	def __init__(self, name, when, foo):
-		self.name = name
-		self.when = when
-		self.do = foo
+    def __init__(self, name, when, foo):
+        self.name = name
+        self.when = when
+        self.do = foo
 
 
 class Crontab:
-	def __init__(self):
-		self.tasks = list()
-		self.thread = Thread(target = self.__check, args = ())
-		self.thread.start()
+    def __init__(self):
+        self.tasks = list()
+        self._thread = Thread(target = self.__check, args = ())
+        self._thread.start()
 
-	def add(self, task):
-		""" Add new task to cron.
-				Task - object of 'Task' class """
-		for t in self.tasks:
-			if t.name == task.name:			# If we alredy have task with this name
-				return -1
-		self.tasks.append(task)
+    def add(self, task):
+        """ Add new task to cron.
+        Task - object of 'Task' class """
+        for t in self.tasks:
+            if t.name == task.name:         # If we alredy have task with this name
+                return -1
+        self.tasks.append(task)
 
-	def remove(self, name):
-		""" Detele task from cron by name """
-		found = None
-		for task in self.tasks:
-			if task.name == name:
-				found = task
-		if found != None:
-			self.tasks.remove(found)
+    def remove(self, name):
+        """ Detele task from cron by name """
+        found = None
+        for task in self.tasks:
+            if task.name == name:
+                found = task
+        if found != None:
+            self.tasks.remove(found)
 
-	def __check(self):
-		""" Cron tick """
-		while True:
-			now = datetime.datetime.now()
-			remove = list()
-			for task in self.tasks:
-				if task.when < now:
-					t = Thread(target = task.do, args= () )
-					t.start()
-					remove.append(task)
-			for task in remove:
-				self.tasks.remove(task)
-			sleep(1)
+    def isExist(self, name):
+        for task in self.tasks:
+            if task.name == name:
+                return True
+        return False
+
+    def __check(self):
+        """ Cron tick """
+        while True:
+            now = datetime.datetime.now()
+            remove = list()
+            for task in self.tasks:
+                if task.when < now:
+                    t = Thread(target = task.do, args= () )
+                    t.start()
+                    remove.append(task)
+            for task in remove:
+                self.tasks.remove(task)
+            sleep(1)
--- smart.py
+++ smart.py
@@ -1,78 +1,103 @@
 # -*- coding: UTF-8 -*-
 
-import objects
-import arduino, os, sys
+import os
+import sys
 import subprocess
 import socket
 from threading import Thread
-import config
-import eeml					# for cosm.com
+import time
+import datetime
+
+import eeml                 # for cosm.com
+
 from log import log
 from log import err
-import time
+import arduino
+import objects
+import config
 
 glob = objects.Vars()
 hole_motion = objects.MotionSensor()
-IR_codes = dict()					# Binded funcitions on IR codes
-repeatable_IR = {b'FFE01F', b'FFA857'}			# This IR codes can be repeated
-last_IR = ''						# Last IR received code
-ultra = None						# subprocess object for radio player
+IR_codes = dict()                   # Binded funcitions on IR codes
+repeatable_IR = {b'FFE01F', b'FFA857'}          # This IR codes can be repeated
+last_IR = ''                        # Last IR received code
+ultra = None                        # subprocess object for radio player
+cron = objects.Crontab()
 
 
 def init_IR_codes():
-	""" Bind functions on IR codes """
-	IR_codes.update({b'FF629D' : say_temp})		# Say temperature status
-	IR_codes.update({b'FFA857' : volume_inc})	# increase volume
-	IR_codes.update({b'FFE01F' : volume_dec})	# reduce volume
-	IR_codes.update({b'FF906F' : radio})		# On/off radio
+    """ Bind functions on IR codes """
+    IR_codes.update({b'FF629D' : say_temp})     # Say temperature status
+    IR_codes.update({b'FFA857' : volume_inc})   # increase volume
+    IR_codes.update({b'FFE01F' : volume_dec})   # reduce volume
+    IR_codes.update({b'FF906F' : radio})        # On/off radio
 
 def cosm_send(id, value):
-	'''Send data to Cosm.com
-	Can't update data yet, so, used recreate class :( '''
-	cosm = eeml.Cosm(config.API_URL, config.API_KEY)
-	cosm.update([eeml.Data(id, value)])
-	try:
-		cosm.put()
-	except eeml.CosmError as e:
-		err('cosm.put(): {}'.format(e))
-	except:
-		err('Unexpected error at cosm.put(): %s' % sys.exc_info()[0])
+    '''Send data to Cosm.com
+    Can't update data yet, so, used recreate class :( '''
+    cosm = eeml.Cosm(config.API_URL, config.API_KEY)
+    cosm.update([eeml.Data(id, value)])
+    try:
+        cosm.put()
+    except eeml.CosmError as e:
+        err('cosm.put(): {}'.format(e))
+    except:
+        err('Unexpected error at cosm.put(): %s' % sys.exc_info()[0])
 
 def volume_dec(value = 200):
-	""" Reduce system volume """
-	subprocess.Popen("amixer set PCM %d-" % (value), shell = True)	
+    """ Reduce system volume """
+    subprocess.Popen("amixer set PCM %d-" % (value), shell = True)
 
 def volume_inc(value = 200):
-	""" Increase system volume """
-	subprocess.Popen("amixer set PCM %d+" % (value), shell = True)	
+    """ Increase system volume """
+    subprocess.Popen("amixer set PCM %d+" % (value), shell = True)
 
 def say(text):
-	""" Text to speech with Google translate """
-	cmd = "wget -q -U Mozilla -O /tmp/say.mp3 \"http://translate.google.com/translate_tts?ie=UTF-8&tl=ru&q=%s\" && mpg123 /tmp/say.mp3" % (text)
-	subprocess.Popen(cmd, shell = True)	
+    """ Text to speech with Google translate """
+    cmd = "wget -q -U Mozilla -O /tmp/say.mp3 \"http://translate.google.com/translate_tts?ie=UTF-8&tl=ru&q=%s\" && mpg123 /tmp/say.mp3" % (text)
+    subprocess.Popen(cmd, shell = True)
 
 def say_temp():
-	""" Report temperatures state """
-	say("Тепература дома %f" % glob.get('T'))
+    """ Report temperatures state """
+    say("Температура дома %f" % glob.get('T'))
 
 def radio():
-	""" On/Off radio """
-	global ultra
-	if ultra  == None:
-		ultra = subprocess.Popen("mpg123 http://94.25.53.132:80/ultra-128.mp3", shell = True)	
-		return
-	else:
-		ultra.kill()
-		ultra = None
+    """ On/Off radio """
+    global ultra
+    if ultra  == None:
+        ultra = subprocess.Popen("mpg123 http://94.25.53.132:80/ultra-128.mp3", shell = True)
+        return
+    else:
+        ultra.kill()
+        ultra = None
 
 def onHoleMotion():
-	log('Motion in hole')
+    log('Motion in hole')
+    if glob.get('noBodyHome') == 1:
+        glob.set('noBodyHome', 0)
+        say('Добро пожаловать домой')
+        say('Текущее время '+datetime.datetime.now().strftime("%H %M"))
+        say_temp()
+        say("Последний раз дома кто-то был " + glob.get('lastMotion').strftime("%H %M"))
+
+
+def onHoleMotionOff():
+    glob.set('lastMotion',datetime.datetime.now())
+    if cron.isExist('noBodyHome'):
+        cron.remove('noBodyHome')
+    cron.add(objects.Task('noBodyHome', \
+                          datetime.datetime.now() + datetime.timedelta(minutes=1), noBodyHome))
+
+def noBodyHome():
+    """ Called by cron when no motion 3 hours"""
+    say('Кажется никого нет дома')
+    glob.set('noBodyHome',1)
 
 def onArduinoFound():
-	say("Связь с Ардуино установлена!")
+    say("Связь с Ардуино установлена!")
 
 def onArduinoLost():
-	say("Утеряна связь с Ардуино! Пытаюсь восстановить связь...")
+    say("Утеряна связь с Ардуино! Пытаюсь восстановить связь...")
 
 
 ####################################################
@@ -80,65 +105,66 @@ def onArduinoLost():
 ####################################################
 
 def dispatch(line):
-	""" Parse serial from Arduino """
-	global last_IR
-	if line[:2] == b'T=':
-		T = float(line.split(b'=')[1])
-		if T != glob.get('T'):
-			glob.set( 'T', T)
-			cosm_send('temp_hole', T)
+    """ Parse serial from Arduino """
+    global last_IR
+    if line[:2] == b'T=':
+        T = float(line.split(b'=')[1])
+        if T != glob.get('T'):
+            glob.set( 'T', T)
+            cosm_send('temp_hole', T)
 
-	elif line == b'hole ON':
-		hole_motion.update(1)
-	elif line == b'hole YES':
-		hole_motion.update(0)
+    elif line == b'hole ON':
+        hole_motion.update(1)
+    elif line == b'hole OFF':
+        hole_motion.update(0)
 
 
-	# cheack if it's 'repeat' IR code and this code repeatable
-	elif (line == b'FFFFFFFF') and ( last_IR in repeatable_IR):
-		line = last_IR		# restore repeated code
+    # cheack if it's 'repeat' IR code and this code repeatable
+    elif (line == b'FFFFFFFF') and ( last_IR in repeatable_IR):
+        line = last_IR      # restore repeated code
 
-	# Check if line in IR code
-	elif line in IR_codes:
-		IR_codes[line]()	# run fuction, binded on this IR code
-		last_IR = line		# remember code for 'repeat' case
+    # Check if line in IR code
+    elif line in IR_codes:
+        IR_codes[line]()    # run fuction, binded on this IR code
+        last_IR = line      # remember code for 'repeat' case
 
 def sock_dispatch(s):
-	data = s.recv(1024)
-	print('sock| ', data)
+    data = s.recv(1024)
+    print('sock| ', data)
 
-	if data == b'radio':
-		radio()
-	s.sendall(b'OK')
-	s.close()
+    if data == b'radio':
+        radio()
+    s.sendall(b'OK')
+    s.close()
 
 def sock_listen():
-	srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	srv.bind(("0.0.0.0", 10000))
-	srv.listen(1)
-	while True:
-					s, addr = srv.accept()
-					print('Connection from', addr)
-					sock_disp_thr = Thread(target = sock_dispatch, args = (s,))
-					sock_disp_thr.start()
+    srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    srv.bind(("0.0.0.0", 10000))
+    srv.listen(1)
+    while True:
+                    s, addr = srv.accept()
+                    print('Connection from', addr)
+                    sock_disp_thr = Thread(target = sock_dispatch, args = (s,))
+                    sock_disp_thr.start()
 
 if __name__ == '__main__':
-	log('Smart home started')
+    log('Smart home started')
 
-	#####     Objects configuration      ######
-	ard = arduino.Arduino('/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A80090sP-if00-port0', onFound = onArduinoFound, onLost = onArduinoLost)
-	hole_motion.onOn = onHoleMotion
-	init_IR_codes()				# init dict: { IR_CODE : function }
-	sock_thr = Thread(target = sock_listen, args = ())
-	sock_thr.start()
+    #####     Objects configuration      ######
+    ard = arduino.Arduino('/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A80090sP-if00-port0', onFound = onArduinoFound, onLost = onArduinoLost)
+    hole_motion.onOn = onHoleMotion
+    hole_motion.onOff = onHoleMotionOff
+    init_IR_codes()             # init dict: { IR_CODE : function }
+    sock_thr = Thread(target = sock_listen, args = ())
+    sock_thr.start()
 
-	#####     main loop     #####
-	while True:
-		try:
-			line = ard.read()			# read line from arduino
-		except KeyboardInterrupt:
-			log('KeyboardInterrupt received. Exit')
-			print('KeyboardInterrupt received. Bye!\n')
-			sys.exit(0)
-		print(line)
-		dispatch(line)
+    #####     main loop     #####
+    while True:
+        try:
+            line = ard.read()           # read line from arduino
+        except KeyboardInterrupt:
+            log('KeyboardInterrupt received. Exit')
+            print('KeyboardInterrupt received. Bye!\n')
+            sys.exit(0)
+        print(line)
+        dispatch(line)
